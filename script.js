aeab0aa5daf99a3393c4613b947232c41a502347
tindog <tindog@gmail.com>
2024-03-21 15:59:28 +0530

    playbar updated

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let currentSong = new Audio;
 
+function formatSeconds(seconds) {
+
+    if (isNaN(seconds) || seconds < 0) {
+        return "Invalid input";
+    }
+
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = Math.floor(seconds % 60);
+
+    // Add leading zero if minutes or seconds are single digit
+    var formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    // var formattedSeconds = Math.floor(remainingSeconds);
+    var formattedSeconds = remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
+
+    return formattedMinutes + ":" + formattedSeconds;
+}
+
 async function getSongs() {
 
     let a = await fetch("http://127.0.0.1:3000/songs/")
@@ -18,16 +35,20 @@ async function getSongs() {
     return songs
 }
 
-const playMusic = (track)=>{
-    // let audio = new Audio("/songs/" + track);
+const playMusic = (track, pause = false) => {
     currentSong.src = "/songs/" + track;
-    currentSong.play();
+    if (!pause) {
+        currentSong.play();
+        play.src = "pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 }
 async function main() {
 
     //Get list of all the songs 
     let songs = await getSongs();
-    console.log(songs);
+    playMusic(songs[0], true);
 
     //Show all the songs in the playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
@@ -46,20 +67,35 @@ async function main() {
     }
 
     //Attach an event listener to each song
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
         e.addEventListener("click", () => {
             playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
         })
     })
 
-    // //Play first song 
-    // var audio = new Audio(songs[0])
-    // audio.play();
+    //Attach an event listener to play, next and previous
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "pause.svg"
+        }
+        else {
+            currentSong.pause();
+            play.src = "playbutton.svg"
+        }
 
-    // audio.addEventListener("loadeddata", () => {
-    //     let duration = audio.duration;
-    //     console.log(duration);
-    // })
+    })
+
+    //listen for timeupdate event
+    currentSong.addEventListener("timeupdate", () => {
+        document.querySelector(".songtime").innerHTML = `${formatSeconds(currentSong.currentTime)} / ${formatSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration)*100 + "%";
+    })
+
+    document.querySelector(".seekbar").addEventListener("click", e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100
+        document.querySelector(".circle").style.left =percent + "%"; 
+        currentSong.currentTime = percent / 100 * currentSong.duration;
+    })
 }
-
 main();
